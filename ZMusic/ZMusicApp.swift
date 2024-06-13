c6a04e59e782f9e0fa45478751f41c1579b24458
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-06-13 22:00:12 +0300

    Initial Commit

---
+++ ZMusic/ZMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMusicApp.swift
+//  ZMusic
+//
+//  Created by Анна on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ZMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
